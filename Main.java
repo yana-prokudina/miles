8fb9bf96a9f54a4290df71d50b31130f14c5f946
Yana Prokudina <Yana@MacBook-Pro-Yana.local>
2020-03-23 21:08:58 +0300

    Java code added

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BigDecimal ticketprice = new BigDecimal("67897");
+        BigDecimal miles = new BigDecimal("20");
+        BigDecimal bonusmiles = ticketprice.divide(miles,0, RoundingMode.FLOOR); // деление
+
+        System.out.println(bonusmiles);
+
+    }
+}
+
+
+
